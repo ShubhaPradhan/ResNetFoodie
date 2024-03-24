648f60a2a7812ff1722791d81598a61748a12951
nordengt <nordenghisingt123@gmail.com>
2024-03-24 20:58:40 +0545

    resnet foodie

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ResNetFoodie
+
+This app is Transfer Learning of ResNet50 for Nepali Food Classification. 
+
+Current supports 8 classes = ['chatamari', 'chhoila', 'dalbhat', 'dhindo', 'gundruk', 'kheer', 'momo', 'sekuwa', 'selroti']
+
+Dataset: https://www.kaggle.com/datasets/saurabkunwar/nepali-food-images
+
+### Plots
+
+Trained for 100 epoch, the model achieved accuracy of 87% during training and 82% on validation and testing datasets.
+
+![Loss and Accuracy plot](results/ResNet.png)
+
+### Inference
+
+![Result 1](images/result1.png)
+
+![Result 2](images/result2.png)
---
+++ dataloader/dataloader.py
@@ -0,0 +1,28 @@
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+
+from .dataset import FoodDataset
+
+def dataloader(batch_size: int = 32):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    train_dataset = FoodDataset(root_dir="../data/NepaliFood/train", transform=transform)
+    test_dataset = FoodDataset(root_dir="../data/NepaliFood/test", transform=transform)
+
+    num_classes = train_dataset.classes
+    train_size = int(0.8*len(train_dataset))
+    val_size = len(train_dataset) - train_size
+
+    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_dataloader, val_dataloader, test_dataloader, len(num_classes)
